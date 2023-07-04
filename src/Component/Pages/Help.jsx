1f8bac520b047e6e5ed49796878c1b51c0ab550e
karim742 <121242355+karim742@users.noreply.github.com>
2023-07-04 16:34:39 +0600

    add useEffect concept

--- src/Component/Pages/Help.jsx
+++ src/Component/Pages/Help.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../Layout/Layout';
 
 function Help() {
+  const [state, setState] = useState({});
+  useEffect(() => {
+    setTimeout(() => {
+      setState({ name: 'Tushar' });
+      console.log('setTimeOut rendering');
+    }, 2000);
+  }, []);
+  console.log('help component rendering');
   return (
     <>
       <Layout>
-        <h1>This is a Help Page</h1>
+        <h1>Hello, {state.name ? state.name : 'Guest'} I am a Help Page</h1>
       </Layout>
     </>
   );
