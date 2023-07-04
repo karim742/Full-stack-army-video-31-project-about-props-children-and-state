d2187100c5af092d496711a2d602b3eea73d626e
karim742 <121242355+karim742@users.noreply.github.com>
2023-07-04 16:56:22 +0600

    1 file modified

--- src/Component/Pages/Help.jsx
+++ src/Component/Pages/Help.jsx
@@ -1,19 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../Layout/Layout';
 
+const data = [
+  {
+    name: 'Abdullah Turky',
+    email: 'turky@test.com',
+  },
+  {
+    name: 'Arjun Roy',
+    email: 'arjun@test.com',
+  },
+  {
+    name: 'Fahim Faisal',
+    email: 'fahim@test.com',
+  },
+  {
+    name: 'Helal',
+    email: 'helal@test.com',
+  },
+  {
+    name: 'Tushar Imran',
+    email: 'tushar@test.com',
+  },
+];
+
 function Help() {
   const [state, setState] = useState({});
   useEffect(() => {
     setTimeout(() => {
       setState({ name: 'Tushar' });
-      console.log('setTimeOut rendering');
     }, 2000);
   }, []);
-  console.log('help component rendering');
   return (
     <>
       <Layout>
         <h1>Hello, {state.name ? state.name : 'Guest'} I am a Help Page</h1>
+        <h2 style={{ margin: '1rem' }}>Some people lists</h2>
+        <ul>
+          {data.length > 0 ? (
+            data.map((man, index) => (
+              <li key={index}>
+                {man.name} - ({man.email})
+              </li>
+            ))
+          ) : (
+            <p>People not found</p>
+          )}
+        </ul>
       </Layout>
     </>
   );
